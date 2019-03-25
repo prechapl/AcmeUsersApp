3716dc8e55c041eb4033ac095c39fc9a0efc862b
Preston Chaplin <preston@New-MacBook-Pro.fios-router.home>
2019-03-25 09:44:08 -0400

    xsfer to dt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Router } from "react-dom-router"
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      users: {}
+      users: {},
+      count: 0
     };
+  }
+
+  getData = (pageId) => {
+    axios.get("").then();
   }
 
   componentDidMount() {
     axios
-      .get("https://acme-users-api.herokuapp.com/api/users")
+      .get(`https://acme-users-api.herokuapp.com/api/users/${id}`)
       .then(res => {
         console.log("res in axios ", res);
         this.setState({ users: res.data.users });
       })
       .catch(err => console.log(err));
   }
+
   render() {
     const users = this.state.users;
     console.log("inRender ", users);
@@ -26,8 +33,10 @@ class App extends Component {
     }
     return (
       <div>
-          <button onClick={}>First</button>
-          <button onClick={}>Prev</button>
+        <button type="button" onClick={this.getData()}>
+          First
+        </button>
+        <button onClick={}>Prev</button>
           <button onClick={}>Next</button>
           <button onClick={}>Last</button>
         <table className="table">
@@ -40,16 +49,14 @@ class App extends Component {
             </tr>
           </thead>
           <tbody>
-            {users
-              ? users.map(user => (
-                  <tr key={user.id}>
-                    <td>{user.firstName}</td>
-                    <td>{user.middleName}</td>
-                    <td>{user.lastName}</td>
-                    <td>{user.email}</td>
-                  </tr>
-                ))
-              : null}
+            {users.map(user => (
+              <tr key={user.id}>
+                <td>{user.firstName}</td>
+                <td>{user.middleName}</td>
+                <td>{user.lastName}</td>
+                <td>{user.email}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
